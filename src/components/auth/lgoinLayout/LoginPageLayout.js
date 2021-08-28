4d264f8702a38d51385c26411f5cac6fe7fb2f05
rumpapl <rumpakucse14@gmail.com>
2021-08-29 00:41:56 +0600

    added Auth-page bg-color

--- src/components/auth/lgoinLayout/LoginPageLayout.js
+++ src/components/auth/lgoinLayout/LoginPageLayout.js
@@ -5,17 +5,19 @@ import './LoginPageLayoutStyle.css';
 const LoginPageLayout = (props) => {
     return (
         <>
-            {props.head}
-            <Container>
-                <Row>
-                    <Col sm={8} className='social-login'>
-                        {props.left}
-                    </Col>
-                    <Col sm={4}>
-                        {props.right}
-                    </Col>
-                </Row>
-            </Container>
+            <div className='auth-screen'>
+                {props.head}
+                <Container>
+                    <Row>
+                        <Col sm={8} className='social-login'>
+                            {props.left}
+                        </Col>
+                        <Col sm={4}>
+                            {props.right}
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </>
     )
 }
