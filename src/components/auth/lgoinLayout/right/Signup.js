307a5319570ab5b4fb25928c6f79dc92acf4b755
rumpapl <rumpakucse14@gmail.com>
2021-09-01 19:51:47 +0600

    added design on signup page( css-style)

--- src/components/auth/lgoinLayout/right/Signup.js
+++ src/components/auth/lgoinLayout/right/Signup.js
@@ -3,13 +3,13 @@ import { useState } from 'react';
 import './SignupStyle.css';
 
 const Signup = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [rePassword, setRePassword] = useState('');
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [rePassword, setRePassword] = useState(null);
 
-    function handleChange(e, elementName){
+    function handleChange(e, elementName) {
         e.preventDefault();
         if (elementName === 'firstName') {
             console.log(e.target.value);
@@ -33,21 +33,52 @@ const Signup = () => {
         }
     }
 
+
     return (
         <>
             <div className='signup-box'>
                 <span className="title-text">Create a New Account</span>
                 <div className="signup-form">
-                        <input type='text' id='firstName' onChange={(e)=>handleChange(e, 'firstName')} value={firstName} placeholder='First Name' className='input-field' />
-                        <br />
-                        <input type='text' id='lastName' onChange={(e)=>handleChange(e, 'lastName')} value={lastName} placeholder='Last Name' className='input-field' />
-                        <br />
-                        <input type='text' id='email' onChange={(e)=>handleChange(e, 'email')} value={email} placeholder='Email Address' className='input-field' />
-                        <br />
-                        <input type='password' id='password' onChange={(e)=>handleChange(e, 'password')}  value={password} placeholder='Password' className='input-field' />
-                        <br />
-                        <input type='password' id='rePassword' onChange={(e)=>handleChange(e, 'rePassword')} value={rePassword} placeholder='Confirm Password' className='input-field' />
-                        
+                    <input
+                        type='text'
+                        id='firstName'
+                        onChange={(e) => handleChange(e, 'firstName')}
+                        value={firstName}
+                        placeholder='First Name'
+                        className='input-field' />
+                    <br />
+                    <input
+                        type='text'
+                        id='lastName'
+                        onChange={(e) => handleChange(e, 'lastName')} v
+                        alue={lastName}
+                        placeholder='Last Name'
+                        className='input-field' />
+                    <br />
+                    <input
+                        type='text'
+                        id='email'
+                        onChange={(e) => handleChange(e, 'email')}
+                        value={email}
+                        placeholder='Email Address'
+                        className='input-field' />
+                    <br />
+                    <input
+                        type='password'
+                        id='password'
+                        onChange={(e) => handleChange(e, 'password')}
+                        value={password}
+                        placeholder='Password'
+                        className='input-field' />
+                    <br />
+                    <input
+                        type='password'
+                        id='rePassword'
+                        onChange={(e) => handleChange(e, 'rePassword')}
+                        value={rePassword}
+                        placeholder='Confirm Password'
+                        className='input-field' />
+
                 </div>
             </div>
         </>
